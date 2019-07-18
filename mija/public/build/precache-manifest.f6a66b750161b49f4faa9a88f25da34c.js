cff46b4c80a477f1593398a7ceb0ab6dbcf25681
rseligm2 <rseligm2@illinois.edu>
2019-07-18 16:54:13 -0500

    added build folder

---
+++ mija/public/build/precache-manifest.f6a66b750161b49f4faa9a88f25da34c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660db2aea06937959bdc5dc5593a5eaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "886bd03fb1846dbcf1c6",
+    "url": "/static/css/main.af303780.chunk.css"
+  },
+  {
+    "revision": "1137eef73e9ec1c47e7f",
+    "url": "/static/js/2.d7b6d9b4.chunk.js"
+  },
+  {
+    "revision": "886bd03fb1846dbcf1c6",
+    "url": "/static/js/main.d3d59639.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1eab123e10eeb8eb0453653875d668f2",
+    "url": "/static/media/aloe_in_beaker.1eab123e.jpg"
+  },
+  {
+    "revision": "ffe0e7af2f07040bd05395c5e434ff90",
+    "url": "/static/media/apple.ffe0e7af.jpg"
+  },
+  {
+    "revision": "8705d8b2fa5eb165e9c5724b05c95802",
+    "url": "/static/media/beakers.8705d8b2.jpg"
+  },
+  {
+    "revision": "26840c2441c43f738368fc949aeee049",
+    "url": "/static/media/cloverflowers.26840c24.jpg"
+  },
+  {
+    "revision": "e44b7874b0e8252220e7ccd94ed903f1",
+    "url": "/static/media/coconut.e44b7874.jpg"
+  },
+  {
+    "revision": "2088c0fa8393e3213b099c309717f8eb",
+    "url": "/static/media/corn.2088c0fa.jpg"
+  },
+  {
+    "revision": "f06ad7c6d606d6a12033a87bf23db22a",
+    "url": "/static/media/cropped_Mija_Full_Logo.f06ad7c6.jpg"
+  },
+  {
+    "revision": "66e4a20570360793ab889440354b9b32",
+    "url": "/static/media/daisy.66e4a205.jpg"
+  },
+  {
+    "revision": "a44e8e73c417257d6ac0203aeef9fb3a",
+    "url": "/static/media/flowers.a44e8e73.jpg"
+  },
+  {
+    "revision": "9a772092656099af047329b4aac52c51",
+    "url": "/static/media/jojoba.9a772092.png"
+  },
+  {
+    "revision": "1e787cabeefd832295af5d02517c92c5",
+    "url": "/static/media/kale.1e787cab.jpg"
+  },
+  {
+    "revision": "0286e1c5be7dcf10399427455263d2de",
+    "url": "/static/media/milk.0286e1c5.jpg"
+  },
+  {
+    "revision": "00ac02765e30545915ecb6f6b6e33b96",
+    "url": "/static/media/rice.00ac0276.jpg"
+  },
+  {
+    "revision": "48ebf9d87400a6e8bf08225290c2f38a",
+    "url": "/static/media/silk.48ebf9d8.jpg"
+  },
+  {
+    "revision": "6f5cffaf15065f527414c873d0ae0487",
+    "url": "/static/media/soy.6f5cffaf.jpg"
+  },
+  {
+    "revision": "9d0e3717f90910ecff1980e4ab32c030",
+    "url": "/static/media/stepfarm.9d0e3717.jpg"
+  },
+  {
+    "revision": "46a8d95ee34608aa984c5036d39a833c",
+    "url": "/static/media/tree.46a8d95e.jpg"
+  },
+  {
+    "revision": "c1f70ae1bb2b6acc90fb9ade2f24c9ba",
+    "url": "/static/media/waves.c1f70ae1.jpg"
+  },
+  {
+    "revision": "25971c771ed5ace78704348b95c7df19",
+    "url": "/static/media/wheat.25971c77.jpg"
+  }
+]);
