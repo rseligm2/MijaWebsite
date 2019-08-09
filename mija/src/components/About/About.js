c182faa6d0e92d49c6f4d95d347755939516ef5d
rseligm2 <rseligm2@illinois.edu>
2019-08-09 13:29:51 -0500

    ingredients change

--- mija/src/components/About/About.js
+++ mija/src/components/About/About.js
@@ -8,10 +8,11 @@ export default function About(){
             <div className="left_frame">
                 <div className="text_frame">
                     <h2 className="ab_header">About Us</h2>
-                    <p className="ab_sub_header">A nature-based micro-shampooery</p>
-                    <p className="ab_info">It started in 2015.  I was getting very dissatisfied with my hair.  I was spending a lot of money on salon hair color and purchasing expensive shampoos from the salon and yet my hair kept getting drier, frizzier and more sparse.  I was using fragrance free facial cleansers because it was better for my skin and wanted to do the same with my hair care products.</p>
-                    <p className="ab_info">I couldn't find a fragrance free shampoo that actually made my hair look and feel better.  So I used my years of education in chemistry and lab sciences to create my own.  I used the Whole Foods Premium Product ingredients guidelines as my starting point.  I looked for nature-based ingredients that were not only sustainable and biodegradable but also gentle and effective.</p>
-                    <p className="ab_info">My hair feels and looks so much better.  Yours will too.  Give it a try and let me know what you think!</p>
+                    <p className="ab_sub_header">Healthier hair, healthier environment.</p>
+                    <p className="ab_info">It started in 2015 when I was very dissatisfied with my hair. I was spending a lot of money on salon hair color and purchasing expensive shampoos and conditioners and yet my hair kept getting drier, frizzier and more sparse.  I was using fragrance free facial cleansers and lotions at the time because it was better for my sensitive skin.  I wanted the same for my hair.</p>
+                    <p className="ab_info">I couldn't find fragrance free hair care that actually made my hair look and feel better. So I used my years of education in chemistry and lab sciences to create my own. I used the Whole Foods Premium Product ingredients guidelines as my starting point.</p>
+                    <p className="ab_info">As a scuba diver, I’ve seen what plastic and chemicals can do to our oceans.  So creating a product that is biodegradable and limits the use of plastic is also very important to me.  I look for nature-based ingredients that are not only sustainable and biodegradable but also gentle and effective.  Our bottles are 100% aluminum (recyclable) and the labels can be easily peeled off because no glues are used.  (We encourage reuse of the pumps.)</p>
+                    <p className="ab_info">Today, my hair feels and looks so much better. Yours will too.</p>
                     <p className="ab_info">-Debi</p>
                     <a href="mailto:info@mijaclean.com"><p className="ab_info">info@mijaclean.com</p></a>
                 </div>
--- mija/src/components/IngredientPage/Modals.js
+++ mija/src/components/IngredientPage/Modals.js
@@ -10,45 +10,44 @@ export const extracts = <div className="modal_container">
     <img src={flowers} alt="circle_img" className="modal_img"/>
     <h1 className="modal_title">Extracts</h1>
     <h3 className="modal_header">Plant-based extracts</h3>
-    <p className="modal_info">Chamomile extract - From the chamomile flower, it is naturally soothing to dry skin which can help maintain a healthy scalp and can enhance natural highlights.</p>
-    <p className="modal_info">Cucumber extract - Naturally high in minerals and potassium, cucumber extract is soothing to skin and scalp  and contains silica which can strengthen hair.</p>
-    <p className="modal_info">Date palm extract - This is an extract rich in hair smoothing and skin softening properties.  It can help the hair appear thicker while improving manageabllity.</p>
-    <p className="modal_info">Flax seed extract - Rich in Omega-3 acids, its moisturizing and can help prevent breakage.  Controls flyaways and can both hydrate and add shine.</p>
-    <p className="modal_info">Gingko biloba extract - The plant contains 49 different antioxidants and is used to prevent environmental damage, smooth hair and helps decrease flyaways.</p>
+    <p className="modal_info">Chamomile extract - The chamomile flower is a natural soother.  Great for dry skin, chamomile extract will maintain a healthy scalp and enhance natural highlights.</p>
+    <p className="modal_info">Cucumber extract - Brimming with minerals and potassium, cucumber extract is soothing to skin and scalp  and contains silica which can strengthen hair.</p>
+    <p className="modal_info">Date palm extract - Rich in hair smoothing and skin softening properties, this extract helps hair appear thicker while improving manageability.</p>
+    <p className="modal_info">Flax seed extract - Rich in Omega-3 fatty acids, this moisturizing extract can help prevent breakage, control flyaways and can add shine.</p>
+    <p className="modal_info">Gingko biloba extract - A bundle of 49 different antioxidants, this extract is used to prevent environmental damage, smooth hair and helps decrease flyaways.</p>
     <p className="modal_info">Green Tea Extract - Powerful anti-oxidants make this extract perfect for scalp health. The naturally occurring caffeine may stimulate circulation in the scalp to encourage hair growth.</p>
-    <p className="modal_info">Guava - Contains vitamins C and A which can benefit those with oily hair.   Naturally rich in omega fatty acids, it also offers conditioning benefits.</p>
-    <p className="modal_info">Sake - Fermented from rice, sake is rich in vitamins, minerals and amino acids, this extract is aimed at moisturizing and nourishing hair.</p>
-    <p className="modal_info">Soy - Enhances natural highlights of blond to light brown hair while adding shine, volume and decreasing tangles.</p>
+    <p className="modal_info">Guava - Loaded with both vitamins C and A, guava extract can benefit those with oily hair because of its astringent qualities.   Naturally rich in omega fatty acids, it also offers conditioning benefits.</p>
+    <p className="modal_info">Sake - Fermented from rice, sake is rich in vitamins, minerals and amino acids.  This extract moisturizes and nourishes hair.</p>
+    <p className="modal_info">Soy - Enhances natural highlights of blond to light brown hair.  It also minimizes tangles while adding shine and volume to your hair.</p>
 </div>;
 
 export const proteins = <div className="modal_container">
     <img src={tree} alt="circle_img" className="modal_img"/>
     <h1 className="modal_title">Hydrolyzed Proteins</h1>
     <h3 className="modal_header">Plant-based proteins</h3>
-    <p className="modal_info">Baobab protein - Made from the seeds of the upside down tree of Africa, this unique combination of hair loving amino acids, offers exceptional damage recovery, conditioning and nourishment for your hair.</p>
-    <p className="modal_info">Jojoba protein - Highly effective to repair and condition damaged hair.  Ideal for adding shine and luster.</p>
-    <p className="modal_info">Kale protein blend - The combination of Kale, Carrot and Lemon proteins, this blend is packed with vitamins and antioxidants for added softness and shine. It also plumps up hair for added volume.</p>
-    <p className="modal_info">Milk Protein - Nutrient rich due to its complete amino acid content, it improves elasticity of hair, easier combing and strength to prevent breakage.</p>
-    <p className="modal_info">Oat Protein - Creates a silky smooth feeling on hair and skin.  Benefits dry and damaged hair and may reduce the appearance of frizz and flyaways.</p>
-    <p className="modal_info">Phytokeratin Protein- A combination of  hydrolyzed proteins from wheat, corn and soybeans mimics human hair amino acids.  Enhances moisture and conditioning to increase strength and elasticity.</p>
-    <p className="modal_info">Quinoa Protein -High amino acid content, quinoa protein encourages damage repair and conditioning.  It also improves color uptake and retention for color processed hair.</p>
+    <p className="modal_info">Baobab protein - One of the strongest natural hair repair proteins, baobab protein is extracted from the seed of the upside-down tree of Africa.  This powerful mix of hair-loving amino acids enhances damage recovery, conditions and nourishes your hair.</p>
+    <p className="modal_info">Jojoba protein - Highly effective in repairing and conditioning damaged hair, jojoba also brings out the shine and luster of your hair.</p>
+    <p className="modal_info">Kale protein blend - The combination of Kale, Carrot and Lemon proteins is jam-\packed with vitamins and antioxidants for added softness, shine and loads of volume.</p>
+    <p className="modal_info">Milk Protein - The highly nutrient rich protein is a fantastic breakage-prevention agent.  It improves elasticity and strength to make your hair easier to comb.</p>
+    <p className="modal_info">Oat Protein - Creates a silky smooth feeling on hair and skin.  Benefits dry and damaged hair and may reduce the appearance of frizz and flyaways.</p>
+    <p className="modal_info">Phytokeratin Protein- A combination of hydrolyzed proteins from wheat, corn and soybeans this complex mimics human hair amino acids.  Enhances moisture and conditioning to increase strength and elasticity.</p>
+    <p className="modal_info">Quinoa Protein - Qunioa works both to improve color uptake and retention while at the same time encouraging damage repair with conditioning.</p>
     <p className="modal_info">Rice Protein - Enhances natural highlights of blond to light brown hair while adding shine and volume. Also helps decrease tangles.</p>
-    <p className="modal_info">Silk Protein - Provides a protective silk barrier that adds luster, body and manageability.</p>
+    <p className="modal_info">Silk Protein - Creates a protective silk barrier adds luster, body and manageability.</p>
 </div>;
 
 export const activeing = <div className="modal_container">
     <img src={cloverflowers} alt="circle_img" className="modal_img"/>
     <h1 className="modal_title">Active Ingredients</h1>
-    <h3 className="modal_header">Inspired by nature</h3>
-    <p className="modal_info">Follicle Stimulating Peptide - Created from amino acids and red clove extract, this compound was created to encourage hair growth.</p>
-    <p className="modal_info">Kera Straight & Curl - A blend of hydrolyzed keratin and Trametes versicolor extract (a type of mushroom). helps repair and strengthen hair while protecting it while styling.</p>
-    <p className="modal_info">Lycopene Bioferment  -  Can be absorbed by the hair fiber to strengthen hair.  It directly nourishes the hair and provides the building blocks it needs to maintain its integrity.</p>
-    <p className="modal_info">Pisum Stivum Peptide - Made from pea protein, its volumizing, conditioning and reduces damage to promote scalp and follicle health.</p>
-    <p className="modal_info">Procutigen Bond -  Derived from chia seeds, this compound helps repairs broken bonds that occur during chemical stress.</p>
-    <p className="modal_info">Procutigen Hold - Made from bamboo protein, it supports hair to hold styles longer while protecting hair from style damage.</p>
-    <p className="modal_info">Rice Curl Complex - Made from tomato and rice extracts, keratin amino acids and enzymes, this complex is designed to protect your hair against the environment while making curls last longer.</p>
-    <p className="modal_info">Split End Complex - Made from rice protein and rice extract, this ingredient works to repair split ends while preventing new ones.</p>
-    <p className="modal_info">Vegan Kerazyme - Made from bamboo and mushroom extracts plus corn, wheat & soy hydrolyzed proteins, this active works to protect against styling and UV damage.</p>
+    <h3 className="modal_header">Nature’s best with a boost</h3>
+    <p className="modal_info">Growth Boost (Follicle Stimulating Peptide) - Derived from amino acids and red clove extract, this compound encourages hair growth.</p>
+    <p className="modal_info">Strength Boost (Kera Straight & Curl) - A blend of hydrolyzed keratin and Trametes versicolor extract (a type of mushroom). It repairs and strengthens your hair and also protects it during styling.</p>
+    <p className="modal_info">Volume Boost (Pisum Stivum Peptide) - Harnessing the power of pea protein, this boost is volumizing, conditioning and promotes scalp and hair follicle health.</p>
+    <p className="modal_info">Repair Boost (Procutigen Bond) -  Extracted from one of the world’s healthiest powerfoods, chia seeds, this compound repairs broken bonds that occur during chemical stress.</p>
+    <p className="modal_info">Heat Protect Boost (Procutigen Hold) - Made from bamboo protein, it supports hair to hold styles longer while protecting hair from style damage.</p>
+    <p className="modal_info">Curl Boost (Rice Curl Complex) - Made from tomato and rice extracts, keratin amino acids and enzymes, this complex is designed to protect your hair against the environment while making both natural curls and styled curls last longer.</p>
+    <p className="modal_info">Split End Boost (Split End Complex) - Made from rice protein and rice extract, this complex works to repair existing split ends while preventing new ones.</p>
+    <p className="modal_info">Protection Boost (Vegan Kerazyme) - Made from bamboo and mushroom extracts plus corn, wheat & soy hydrolyzed proteins, this complex protects against styling and UV damage.</p>
 </div>;
 
 export const globalconsc = <div className="modal_container">
