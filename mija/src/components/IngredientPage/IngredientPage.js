ae8ffa7d419e3cc240bc5cf6ff3adf175de82e86
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2019-09-03 15:31:17 +0530

    fix ingredients page modals

--- mija/src/components/IngredientPage/IngredientPage.js
+++ mija/src/components/IngredientPage/IngredientPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./IngredientPage.css";
+
 import wheat from '../../resources/wheat.jpg';
 import apple from '../../resources/apple.jpg';
 import daisy from '../../resources/daisy.jpg';
@@ -12,7 +12,7 @@ import Paper from '@material-ui/core/Paper';
 import Dialog from '@material-ui/core/Dialog';
 import Typography from '@material-ui/core/Typography';
 
-import {extracts, proteins, activeing, globalconsc} from './Modals';
+import ModalBody from './Modals';
 
 const useStyles = makeStyles(theme => ({
     headerFrame: {
@@ -68,46 +68,29 @@ const useStyles = makeStyles(theme => ({
     },    
 }));
 
-const modals = [extracts, proteins, activeing, globalconsc];
 
 export default function IngredientPage() {
     const [dialogOpen, setOpen] = useState(false);
-    const [modalIndex, setIndex] = useState(1);
+    const [modalIndex, setIndex] = useState(0);
     const classes = useStyles();
     const isMobile = useMediaQuery('(max-width:600px)');
 
-    function handleClickOpen() {
+    function handleClickOpen(i) {
+        setIndex(i);
         setOpen(true);
     }
-    function handleClickClose() {
-        setOpen(false);
-    }
-    function clickExtracts(){
-        setIndex(0);
-        handleClickOpen();
-    }
-    function clickProteins(){
-        setIndex(1);
-        handleClickOpen();
-    }
-    function clickActive(){
-        setIndex(2);
-        handleClickOpen();
-    }
-    function clickGlobal(){
-        setIndex(3);
-        handleClickOpen();
-    }
 
-    return(
+    const handleClickClose = () => setOpen(false);
+
+    return (
         <div>
             <div className={classes.headerFrame}>
                 <Typography variant="h3" className={classes.ingpageHeader}>NATURAL INGREDIENTS</Typography>
                 <Typography variant="h5" className={classes.ingpageHeader}>sustainable, renewable base formula</Typography>
                 <Typography variant="body1" className={classes.ingpageBaseInfo}>Purified Water, Aloe barbadensis (Aloe Vera) Leaf Juice, Cocoglucoside, Sodium Cocoyl Apple Amino Acids, Sodium CocoylIsethionate, Sodium Cocoyl Hydrolyzed Soy Protein, Propanediol, Panthenol, Glycerin, Simmondsia chinensis (Jojoba) Esters, Xanthan Gum, Sclerotium Gum, Cetyl Alcohol, Phenoxyethanol, Caprylyl Glycol, Citric Acid, Trisodium Ethylenediamine Disuccinate, Sorbic Acid, Lactic Acid</Typography>
             </div>
-            <Dialog open={dialogOpen} onClose={handleClickClose} maxWidth='md' fullWidth={true}>
-                {modals[modalIndex]}
+            <Dialog open={dialogOpen} onClose={handleClickClose} maxWidth='md' fullWidth={true} fullScreen={isMobile}>
+                <ModalBody handleClose={handleClickClose} index={modalIndex} />
             </Dialog>
             <Grid container>
                 <Grid item xs={6} md={3}>
@@ -116,7 +99,7 @@ export default function IngredientPage() {
                     </Paper>
                 </Grid>
                 <Grid item xs={6} md={3}>
-                    <Paper className={classes.paper} onClick={clickExtracts} square>
+                    <Paper className={classes.paper} onClick={() => handleClickOpen(0)} square>
                         <Typography variant={isMobile ? "h6" : "h5"} className={classes.boxHeader}>NATURAL EXTRACTS</Typography>
                         <Typography variant={isMobile ? "body2" : "body1"} className={classes.boxText}>Extracts from nature that are kinder to your hair.</Typography>
                     </Paper>
@@ -127,13 +110,13 @@ export default function IngredientPage() {
                     </Paper>
                 </Grid>
                 <Grid item xs={6} md={3}>
-                    <Paper className={classes.paper} onClick={clickProteins} square>
+                    <Paper className={classes.paper} onClick={() => handleClickOpen(1)} square>
                         <Typography variant={isMobile ? "h6" : "h5"} className={classes.boxHeader}>HYDROLYZED PROTEINS</Typography>
                         <Typography variant={isMobile ? "body2" : "body1"} className={classes.boxText}>Nourishing your hair with essential amino acids.</Typography>
                     </Paper>
                 </Grid>
                 <Grid item xs={6} md={3}>
-                    <Paper className={classes.paper} onClick={clickActive} square>
+                    <Paper className={classes.paper} onClick={() => handleClickOpen(2)} square>
                         <Typography variant={isMobile ? "h6" : "h5"} className={classes.boxHeader}>TARGETED ACTIVE INGREDIENTS</Typography>
                         <Typography variant={isMobile ? "body2" : "body1"} className={classes.boxText}>Nature’s best with a boost!</Typography>
                     </Paper>
@@ -144,7 +127,7 @@ export default function IngredientPage() {
                     </Paper>
                 </Grid>
                 <Grid item xs={6} md={3}>
-                    <Paper className={classes.paper} onClick={clickGlobal} square>
+                    <Paper className={classes.paper} onClick={() => handleClickOpen(3)} square>
                         <Typography variant={isMobile ? "h6" : "h5"} className={classes.boxHeader}>GLOBALLY CONCIOUS</Typography>
                         <Typography variant={isMobile ? "body2" : "body1"} className={classes.boxText}>ECOCERT, organic, biodegradable</Typography>
                     </Paper>
